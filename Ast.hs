16f7114f7a3aed363ef27d4b28f775fee15627b7
david <david@pako.st>
2009-12-27 09:40:37 +0100

    towards a sane alpha

--- Ast.hs
+++ Ast.hs
@@ -1,9 +1,10 @@
 module Ast
-    ( Term, VarID, SpliceID
+    ( Term, VarID, Vars, SpliceID
     , AST(..), ast
-    , var, app, lam, leet
-    , freeVars, freeIn
+    , var, app, lam, fixLet
+    , freeVars, freeIn, notFreeIn, vars, varIn
     ) where
+
 
 import Prelude hiding ( elem )
 
@@ -16,39 +17,56 @@ type SpliceID = String
 
 
 data Term = Var_ VarID
-          | App_ Vars Term Term
-          | Lam_ Vars VarID Term
-          | Let_ Vars VarID Term Term
-          | Splice_ Vars Vars SpliceID
+          | App_ Vars Vars Term Term
+          | Lam_ Vars Vars VarID Term
+          | Let_ Vars Vars VarID Term Term
+          -- | Splice_ Vars Vars Vars SpliceID
     -- deriving (Show, Eq)
     deriving (Eq)
 
 
 freeVars :: Term -> Vars
 freeVars (Var_ x) = singleton x
-freeVars (App_ xs _ _) = xs
-freeVars (Lam_ xs _ _) = xs
-freeVars (Let_ xs _ _ _) = xs
-freeVars (Splice_ xs _ _) = xs
+freeVars (App_ xs _ _ _) = xs
+freeVars (Lam_ xs _ _ _) = xs
+freeVars (Let_ xs _ _ _ _) = xs
+-- freeVars (Splice_ xs _ _) = xs
+
+vars :: Term -> Vars
+vars (Var_ x) = singleton x
+vars (App_ _ xs _ _) = xs
+vars (Lam_ _ xs _ _) = xs
+vars (Let_ _ xs _ _ _) = xs
 
 
-freeIn :: VarID -> Term -> Bool
+freeIn, notFreeIn :: VarID -> Term -> Bool
 freeIn x t = x `elem` freeVars t
+notFreeIn x t = not (freeIn x t)
+
+varIn :: VarID -> Term -> Bool
+varIn x t = x `elem` vars t
 
 
 var :: VarID -> Term
 var = Var_
 
 app :: Term -> Term -> Term
-app a b = App_ (freeVars a `union` freeVars b) a b
+app a b = App_ (freeVars a `union` freeVars b)
+               (vars a `union` vars b)
+               a b
 
 lam :: VarID -> Term -> Term
-lam x m = Lam_ (x `remove` freeVars m) x m
+lam x m = Lam_ (x `remove` freeVars m)
+               (x `insert` vars m)
+               x m
 
-leet :: VarID -> Term -> Term -> Term
-leet x e m | x `freeIn` m = Let_ nufree x e m
-           | otherwise = m
+
+fixLet :: VarID -> Term -> Term -> Term
+fixLet x e m = Let_ nufree nuvars x e m
     where nufree = x `remove` (freeVars e `union` freeVars m)
+          nuvars = x `insert` (vars e `union` vars m)
+
+
 
 
 data AST = Var VarID
@@ -61,9 +79,9 @@ data AST = Var VarID
 
 ast :: Term -> AST
 ast (Var_ x) = Var x
-ast (App_ _ l r) = App l r
-ast (Lam_ _ x m) = Lam x m
-ast (Let_ _ x e m) = Let x e m
+ast (App_ _ _ l r) = App l r
+ast (Lam_ _ _ x m) = Lam x m
+ast (Let_ _ _ x e m) = Let x e m
 
 
 
--- play.hs
+++ play.hs
@@ -5,13 +5,15 @@ import Stringy
 import Operations
 
 
-problem1, problem2 :: Term
+problem1, problem2, problem3, problem4 :: Term
 problem1 = read "(let (a = λq.q) (b = λo.o) a b c) P Q"
 problem2 = read "(λf.(λw.f (w w)) λu.f (u u)) (λsx.s) P Q R"
+problem3 = read "let (y = λf.f (y f)) y (λsx.s x x) A"
+problem4 = read "(λf.(λw.f (w w)) λu.f (u u)) (λsx.s x x) A"
 
-test3, test4 :: Term -> IO ()
-test3 = mapM_ print . take 50 . whnf1
-test4 = mapM_ print . take 50 . whnf2
+test1, test2 :: Term -> IO ()
+test1 = mapM_ print . take 50 . whnf1
+test2 = mapM_ print . take 50 . whnf2
 
 
 main = print (whnf2 problem2 !! 1000)
